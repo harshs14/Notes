6cb6add2234afb959e2c8cd8515b2b1961a50705
harshs14 <harsh.singh1411@gmail.com>
2020-12-21 19:22:01 +0530

    notes model

--- server/note/models.py
+++ server/note/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Notes(models.Model):
+    title = models.CharField(max_length=30, unique=True, null=False)
+    content = models.TextField(max_length=1000)
+    createdOn = models.DateTimeField(default=timezone.now)
+    modifiedOn = models.DateTimeField(default=timezone.now)
+    bookmark = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
