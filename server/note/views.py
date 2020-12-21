c98078a46cf05e23afa3c8cd227ee9be927f4a7d
harshs14 <harsh.singh1411@gmail.com>
2020-12-21 21:24:47 +0530

    bookmarked notes

--- server/note/views.py
+++ server/note/views.py
@@ -1,12 +1,17 @@
 from rest_framework.views import APIView
 from .models import Notes
-from rest_framework import permissions, status, viewsets
+from rest_framework import permissions, viewsets
 from .serializers import NotesSerializer
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 class NotesView(viewsets.ModelViewSet):
     serializer_class = NotesSerializer
     permission_classes = (permissions.AllowAny,)
     queryset = Notes.objects.all()
 
-
+    @action(methods=['GET'], detail=False)
+    def bookmarks(self, request, *args, **kwargs):
+        bookmarkedNotes = Notes.objects.filter(bookmark=True)
+        serializer = NotesSerializer(bookmarkedNotes, many=True)
+        return Response(serializer.data)
